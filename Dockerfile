2bfb07b61beb9d9d2c39e578d0517e94b4b58ee7
massalino <fabiomassalino@gmail.com>
2020-12-29 17:43:51 -0300

    Adicionando testes

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,10 @@ COPY api/carta.js /var/task/cartas-papainoel/api/
 WORKDIR /var/task/cartas-papainoel
 RUN npm install --save bluebird
 RUN npm install --save uuid
+RUN npm install mocha -g --save-dev
+RUN npm install chai --save-dev
+RUN npm install should --save-dev
+RUN npm install request --save-dev
+RUN mkdir /var/task/cartas-papainoel/test
+COPY test/carta-test.js /var/task/cartas-papainoel/test/
 CMD ["node"]
---
+++ test/carta-test.js
@@ -0,0 +1,38 @@
+var should = require("should");
+var request = require("request");
+var chai = require("chai");
+var expect = chai.expect;
+var urlBase = "https://hdrivvglei.execute-api.us-east-1.amazonaws.com/dev";
+
+
+describe("Teste API carta-papainoel listar ",function(){
+  
+  it("Deve listar as cartas",function(done){
+    request.get(
+      {
+        url : urlBase + "/listar"
+      },
+      function(error, response, body){
+  
+        var _body = {};
+        try{
+          _body = JSON.parse(body);
+        }
+        catch(e){
+          _body = {};
+        }
+
+        // utilizando a funcao expect do chai, vamos verificar se o resultado da chamada foi sucesso (200)
+        expect(response.statusCode).to.equal(200);
+
+        // agora, verificamos se retornou a propriedade carta
+        if( _body.should.have.property('carta') ){
+          //se retornou, verificar se tem ao menos 10 cartas 
+          expect(_body.carta).to.have.lengthOf.at.above(10);
+        }
+        done(); 
+      }
+    );
+  });
+  
+});
